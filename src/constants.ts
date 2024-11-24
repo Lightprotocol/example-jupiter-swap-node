09e10907ab6b78fb19b9f705d61ee071b025f487
Swenschaeferjohann <swen-code@swens-MacBook-Air-2.local>
2024-11-24 15:56:03 +0000

    format with .prettierrc

--- src/constants.ts
+++ src/constants.ts
@@ -1,29 +1,29 @@
-import { Keypair, PublicKey } from "@solana/web3.js";
-import fs from "fs";
-import dotenv from "dotenv";
-import os from "os";
-import { QuoteGetRequest, SwapRequest } from "@jup-ag/api";
+import { Keypair, PublicKey } from '@solana/web3.js';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import os from 'os';
+import { QuoteGetRequest, SwapRequest } from '@jup-ag/api';
 
 dotenv.config();
 
-if (!process.env.INPUT_MINT) throw new Error("INPUT_MINT is not set");
+if (!process.env.INPUT_MINT) throw new Error('INPUT_MINT is not set');
 if (!process.env.SWAP_USER_KEYPAIR_PATH)
-  throw new Error("SWAP_USER_KEYPAIR_PATH is not set");
+    throw new Error('SWAP_USER_KEYPAIR_PATH is not set');
 if (!process.env.LIGHT_LUT && !process.env.LIGHT_LUT_DEVNET)
-  throw new Error("LIGHT_LUT and LIGHT_LUT_DEVNET is not set");
+    throw new Error('LIGHT_LUT and LIGHT_LUT_DEVNET is not set');
 
-if (!process.env.RPC_URL) throw new Error("RPC_URL is not set");
-if (!process.env.COMPRESSION_URL) throw new Error("COMPRESSION_URL is not set");
+if (!process.env.RPC_URL) throw new Error('RPC_URL is not set');
+if (!process.env.COMPRESSION_URL) throw new Error('COMPRESSION_URL is not set');
 
 export const swapUserKeypair = Keypair.fromSecretKey(
-  Buffer.from(
-    JSON.parse(
-      fs.readFileSync(
-        process.env.SWAP_USER_KEYPAIR_PATH!.replace("~", os.homedir()),
-        "utf8"
-      )
-    )
-  )
+    Buffer.from(
+        JSON.parse(
+            fs.readFileSync(
+                process.env.SWAP_USER_KEYPAIR_PATH!.replace('~', os.homedir()),
+                'utf8',
+            ),
+        ),
+    ),
 );
 export const payerPublicKey = swapUserKeypair.publicKey;
 export const inputMint = new PublicKey(process.env.INPUT_MINT!);
@@ -32,31 +32,31 @@ export const compressionUrl = process.env.COMPRESSION_URL!;
 export const rpcUrl = process.env.RPC_URL!;
 export const lightLut = process.env.LIGHT_LUT!;
 
-console.log("payerPublicKey: ", payerPublicKey.toBase58());
-console.log("inputMint: ", inputMint.toBase58());
-console.log("outputMint: ", outputMint.toBase58());
+console.log('payerPublicKey: ', payerPublicKey.toBase58());
+console.log('inputMint: ', inputMint.toBase58());
+console.log('outputMint: ', outputMint.toBase58());
 
 export const amount = 1042; // 0.001042 usdc
 export const swapConfig: QuoteGetRequest = {
-  inputMint: inputMint.toBase58(),
-  outputMint: outputMint.toBase58(),
-  amount,
-  maxAccounts: 64,
-  onlyDirectRoutes: true,
-  restrictIntermediateTokens: true,
-  slippageBps: 500,
-  computeAutoSlippage: true,
-  minimizeSlippage: true,
-  preferLiquidDexes: true,
+    inputMint: inputMint.toBase58(),
+    outputMint: outputMint.toBase58(),
+    amount,
+    maxAccounts: 64,
+    onlyDirectRoutes: true,
+    restrictIntermediateTokens: true,
+    slippageBps: 500,
+    computeAutoSlippage: true,
+    minimizeSlippage: true,
+    preferLiquidDexes: true,
 };
 
 export const swapRequestConfig: SwapRequest = {
-  userPublicKey: null!, // set in handleSend
-  quoteResponse: null!, // set in handleSend
-  skipUserAccountsRpcCalls: false,
-  dynamicComputeUnitLimit: true,
-  wrapAndUnwrapSol: true,
-  prioritizationFeeLamports: 100_000,
+    userPublicKey: null!, // set in handleSend
+    quoteResponse: null!, // set in handleSend
+    skipUserAccountsRpcCalls: false,
+    dynamicComputeUnitLimit: true,
+    wrapAndUnwrapSol: true,
+    prioritizationFeeLamports: 100_000,
 };
 
-console.log("swapConfig: ", swapConfig);
+console.log('swapConfig: ', swapConfig);
--- src/handleSend.ts
+++ src/handleSend.ts
@@ -2,161 +2,164 @@
 /// MIT License
 /// For purpose of demoing compressed token swaps with the Jupiter API
 /// Added logToFile()
-import { Rpc, sleep } from "@lightprotocol/stateless.js";
+import { Rpc, sleep } from '@lightprotocol/stateless.js';
 import {
-  BlockhashWithExpiryBlockHeight,
-  Connection,
-  TransactionExpiredBlockheightExceededError,
-  VersionedTransactionResponse,
-} from "@solana/web3.js";
-import promiseRetry from "promise-retry";
-import { logToFile } from "./logger.ts";
+    BlockhashWithExpiryBlockHeight,
+    Connection,
+    TransactionExpiredBlockheightExceededError,
+    VersionedTransactionResponse,
+} from '@solana/web3.js';
+import promiseRetry from 'promise-retry';
+import { logToFile } from './logger.ts';
 
 type TransactionSenderAndConfirmationWaiterArgs = {
-  connection: Connection;
-  serializedTransaction: Buffer;
-  blockhashWithExpiryBlockHeight: BlockhashWithExpiryBlockHeight;
-  debug: boolean;
+    connection: Connection;
+    serializedTransaction: Buffer;
+    blockhashWithExpiryBlockHeight: BlockhashWithExpiryBlockHeight;
+    debug: boolean;
 };
 
 const SEND_OPTIONS = {
-  skipPreflight: true,
+    skipPreflight: true,
 };
 
 async function transactionSenderAndConfirmationWaiter({
-  connection,
-  serializedTransaction,
-  blockhashWithExpiryBlockHeight,
-  debug,
-}: TransactionSenderAndConfirmationWaiterArgs): Promise<VersionedTransactionResponse | null> {
-  logToFile("Sending transaction...", debug);
-  const txid = await connection.sendRawTransaction(
+    connection,
     serializedTransaction,
-    SEND_OPTIONS
-  );
-  logToFile(`Transaction sent with id: ${txid}`, debug);
+    blockhashWithExpiryBlockHeight,
+    debug,
+}: TransactionSenderAndConfirmationWaiterArgs): Promise<VersionedTransactionResponse | null> {
+    logToFile('Sending transaction...', debug);
+    const txid = await connection.sendRawTransaction(
+        serializedTransaction,
+        SEND_OPTIONS,
+    );
+    logToFile(`Transaction sent with id: ${txid}`, debug);
 
-  const controller = new AbortController();
-  const abortSignal = controller.signal;
+    const controller = new AbortController();
+    const abortSignal = controller.signal;
 
-  const abortableResender = async () => {
-    while (true) {
-      await sleep(2_000);
-      if (abortSignal.aborted) return;
-      try {
-        logToFile("Resending transaction...", debug);
-        await connection.sendRawTransaction(
-          serializedTransaction,
-          SEND_OPTIONS
+    const abortableResender = async () => {
+        while (true) {
+            await sleep(2_000);
+            if (abortSignal.aborted) return;
+            try {
+                logToFile('Resending transaction...', debug);
+                await connection.sendRawTransaction(
+                    serializedTransaction,
+                    SEND_OPTIONS,
+                );
+            } catch (e) {
+                logToFile(`Failed to resend transaction: ${e}`, debug);
+            }
+        }
+    };
+
+    try {
+        abortableResender();
+        const lastValidBlockHeight =
+            blockhashWithExpiryBlockHeight.lastValidBlockHeight - 50; // waits 100 blocks
+        logToFile(
+            `Waiting for confirmation with lastValidBlockHeight: ${lastValidBlockHeight}`,
+            debug,
         );
-      } catch (e) {
-        logToFile(`Failed to resend transaction: ${e}`, debug);
-      }
-    }
-  };
 
-  try {
-    abortableResender();
-    const lastValidBlockHeight =
-      blockhashWithExpiryBlockHeight.lastValidBlockHeight - 50; // waits 100 blocks
-    logToFile(
-      `Waiting for confirmation with lastValidBlockHeight: ${lastValidBlockHeight}`,
-      debug
+        // this would throw TransactionExpiredBlockheightExceededError
+        await Promise.race([
+            connection.confirmTransaction(
+                {
+                    ...blockhashWithExpiryBlockHeight,
+                    lastValidBlockHeight,
+                    signature: txid,
+                    abortSignal,
+                },
+                'confirmed',
+            ),
+            new Promise(async resolve => {
+                // in case ws socket died
+                while (!abortSignal.aborted) {
+                    await sleep(2_000);
+                    logToFile('Checking transaction status...', debug);
+                    const tx = await connection.getSignatureStatus(txid, {
+                        searchTransactionHistory: false,
+                    });
+                    if (tx?.value?.confirmationStatus === 'confirmed') {
+                        logToFile(
+                            'Transaction confirmed via status check',
+                            debug,
+                        );
+                        resolve(tx);
+                    }
+                }
+            }),
+        ]);
+    } catch (e) {
+        if (e instanceof TransactionExpiredBlockheightExceededError) {
+            logToFile('Transaction expired, returning null', debug);
+            // we consume this error and getTransaction would return null
+            return null;
+        } else {
+            // invalid state from web3.js
+            logToFile(`Unexpected error: ${e}`, debug);
+            throw e;
+        }
+    } finally {
+        logToFile('Aborting transaction monitoring', debug);
+        controller.abort();
+    }
+
+    // in case rpc is not synced yet, we add some retries
+    logToFile('Getting final transaction details with retries...', debug);
+    const response = promiseRetry(
+        async retry => {
+            const response = await connection.getTransaction(txid, {
+                commitment: 'confirmed',
+                maxSupportedTransactionVersion: 0,
+            });
+            if (!response) {
+                logToFile('Transaction not found, retrying...', debug);
+                retry(response);
+            }
+            return response;
+        },
+        {
+            retries: 4,
+            minTimeout: 1e3,
+        },
     );
 
-    // this would throw TransactionExpiredBlockheightExceededError
-    await Promise.race([
-      connection.confirmTransaction(
-        {
-          ...blockhashWithExpiryBlockHeight,
-          lastValidBlockHeight,
-          signature: txid,
-          abortSignal,
-        },
-        "confirmed"
-      ),
-      new Promise(async (resolve) => {
-        // in case ws socket died
-        while (!abortSignal.aborted) {
-          await sleep(2_000);
-          logToFile("Checking transaction status...", debug);
-          const tx = await connection.getSignatureStatus(txid, {
-            searchTransactionHistory: false,
-          });
-          if (tx?.value?.confirmationStatus === "confirmed") {
-            logToFile("Transaction confirmed via status check", debug);
-            resolve(tx);
-          }
-        }
-      }),
-    ]);
-  } catch (e) {
-    if (e instanceof TransactionExpiredBlockheightExceededError) {
-      logToFile("Transaction expired, returning null", debug);
-      // we consume this error and getTransaction would return null
-      return null;
-    } else {
-      // invalid state from web3.js
-      logToFile(`Unexpected error: ${e}`, debug);
-      throw e;
-    }
-  } finally {
-    logToFile("Aborting transaction monitoring", debug);
-    controller.abort();
-  }
-
-  // in case rpc is not synced yet, we add some retries
-  logToFile("Getting final transaction details with retries...", debug);
-  const response = promiseRetry(
-    async (retry) => {
-      const response = await connection.getTransaction(txid, {
-        commitment: "confirmed",
-        maxSupportedTransactionVersion: 0,
-      });
-      if (!response) {
-        logToFile("Transaction not found, retrying...", debug);
-        retry(response);
-      }
-      return response;
-    },
-    {
-      retries: 4,
-      minTimeout: 1e3,
-    }
-  );
-
-  logToFile("Transaction processing complete", debug);
-  return response;
+    logToFile('Transaction processing complete', debug);
+    return response;
 }
 
 export async function handleSend(
-  connection: Rpc,
-  serializedTransaction: Buffer,
-  blockhash: string,
-  lastValidBlockHeight: number,
-  debug: boolean
+    connection: Rpc,
+    serializedTransaction: Buffer,
+    blockhash: string,
+    lastValidBlockHeight: number,
+    debug: boolean,
 ) {
-  const transactionResponse = await transactionSenderAndConfirmationWaiter({
-    connection,
-    serializedTransaction,
-    blockhashWithExpiryBlockHeight: {
-      blockhash,
-      lastValidBlockHeight,
-    },
-    debug,
-  });
+    const transactionResponse = await transactionSenderAndConfirmationWaiter({
+        connection,
+        serializedTransaction,
+        blockhashWithExpiryBlockHeight: {
+            blockhash,
+            lastValidBlockHeight,
+        },
+        debug,
+    });
 
-  // If we are not getting a response back, the transaction has not confirmed.
-  if (!transactionResponse) {
-    console.error("Transaction not confirmed");
-    return;
-  }
+    // If we are not getting a response back, the transaction has not confirmed.
+    if (!transactionResponse) {
+        console.error('Transaction not confirmed');
+        return;
+    }
 
-  if (transactionResponse.meta?.err) {
-    console.error(transactionResponse.meta?.err);
-  }
+    if (transactionResponse.meta?.err) {
+        console.error(transactionResponse.meta?.err);
+    }
 
-  console.log(
-    `https://solscan.io/tx/${transactionResponse.transaction.signatures[0]}`
-  );
+    console.log(
+        `https://solscan.io/tx/${transactionResponse.transaction.signatures[0]}`,
+    );
 }
